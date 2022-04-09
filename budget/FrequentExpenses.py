db69116aeb6fd4cc92ec04267f3fe46166e9154f
Patrick Desmarais <patrick.desm@gmail.com>
2022-04-09 19:56:19 -0400

    M1 .. counting most common cats

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,4 +1,20 @@
+import collections
 from . import Expense
 
 expenses = Expense.Expenses()
 expenses.read_expenses("data/spending_data.csv")
+
+spending_categories = []
+for expense in expenses.list:
+    # if expense.category in spending_categories:
+    #    continue
+    spending_categories.append(expense.category)
+
+spending_counter = collections.Counter(spending_categories)
+top5 = spending_counter.most_common(5)
+
+# zip can combine lists
+# zip can take a dictionary and separate keys and values (note the * symbol):
+categories, count = zip(*top5)
+print(categories)
+print(count)
