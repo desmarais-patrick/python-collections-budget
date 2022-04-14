61a08e54e08ff9ba15e5f267e380ae7a8fffdecd
Patrick Desmarais <patrick.desm@gmail.com>
2022-04-14 14:09:08 -0400

    Add comments to understand zip fn

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -14,8 +14,13 @@ for expense in expenses.list:
 
 spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
+#print("Top 5:", top5)
+#print("Decomposed:", *top5)
+#for i in zip(*top5):
+#    print("Zipped'i:", i)
 
-# zip can combine lists
+# zip can combine lists i-th elements, like a zipper meeting two or more pins,
+#     ex. zip((1, 2), ("a", "b")) ==> (1, "a"), (2, "b")
 # zip can take a dictionary and separate keys and values (note the * symbol):
 categories, count = zip(*top5)
 
