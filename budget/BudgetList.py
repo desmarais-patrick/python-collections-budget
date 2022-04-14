7a9e6abe8ae22b6649d96dbc20839c8ad2c0ac6e
Patrick Desmarais <patrick.desm@gmail.com>
2022-04-14 14:29:23 -0400

    Complete BudgetList class and add main

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,2 +1,32 @@
+from . import Expense
+
 class BudgetList:
-    pass
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+        # Define "overage" : an excess or surplus, especially the amount by which a sum of money is greater than a previous estimate.
+    def append(self, item):
+        if self.sum_expenses + item < self.budget:
+            self.expenses.append(item)
+            self.sum_expenses += item
+        else:
+            self.overages.append(item)
+            self.sum_overages += item
+
+    def __len__(self):
+        return len(self.expenses) + len(self.overages)
+
+
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses("data/spending_data.csv")
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+    print("The count of all expenses: ", str(len(myBudgetList)))
+
+if __name__ == "__main__":
+    main()
